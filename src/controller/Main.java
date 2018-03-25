8d9c1b3675b9da1790540391b67dd94dc67aa37b
GHFIGH <1606526833@qq.com>
2018-03-25 15:28:32 +0800

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -13,28 +13,14 @@ import util.VersionUtil;
 /**
  * 
  * @author Sun
- * @date 2018年3月20日 下午3:05:15
+ * @date 2018脛锚3脭脗20脠脮 脧脗脦莽3:05:15
  */
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		List<String> list = ReadUtil.readClassFile("E://Main.class");
 		// List<String> list = ReadUtil.readClassFile("File Name");
-		boolean isClass = ClassUtil.isIllegalClass(list);
-		if (!isClass) {
-			throw new Exception("Not a class file");
-		}
-		VersionUtil.getVersion(list);
-		ConstantPoolUtil constantPool = new ConstantPoolUtil(list);
-		int num = constantPool.info();
-		AccessFlagsUtil accessFlagsUtil = new AccessFlagsUtil(list, num);
-		num = accessFlagsUtil.info();
-		ClassUtil classUtil = new ClassUtil(list, num);
-		num = classUtil.info();
-		FieldsUtils fieldsUtils = new FieldsUtils(num, list);
-		num = fieldsUtils.info();
-		MethodUtil methodUtil = new MethodUtil(num, list);
-		methodUtil.info();
+		// 鑷璋冪敤
 	}
 
 }
